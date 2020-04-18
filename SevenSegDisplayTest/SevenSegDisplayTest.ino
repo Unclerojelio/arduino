ff4c738307e0c59a42e789044c262ebff4ab6e6e
Roger Banks <roger_banks@mac.com>
2020-04-17 19:38:47 -0500

    Corrected pin mapping for ATTiny24. Corrected comments to reflect correct mapping.

--- SevenSegDisplayTest/SevenSegDisplayTest.ino
+++ SevenSegDisplayTest/SevenSegDisplayTest.ino
@@ -1,15 +1,17 @@
 // Display the 15 digits 0-F on a seven-segment display.
 // This sketch will eventually be the basis for a sketch to be used on
-// ATTiny25 microcontrollers to create the behavior of BCD-to-Seven-segment
+// ATTiny24 microcontrollers to create the behavior of BCD-to-Seven-segment
 // display drivers.
 //
 // Roger Banks
 // 2020 04 15
 //
+// 2020 04 17: Code loaded on ATTiny24 and driving display
+//
 // FND503 Common Cathod Seven Segment Display
 //
-//       1
-//       0  9  G  4  5     <-- Arduino pins
+//       
+//       6  5  G  0  1     <-- Arduino pins
 //       |  |  |  |  |
 //       g  f     a  b
 //     +---------------+
@@ -21,24 +23,24 @@
 //     |      ---      |3
 //     |       d       |
 //     +---------------+
-//       e  d     d
+//       e  d     c
 //       |  |  |  |  |
-//       8  6  G  7  N    <-- Arduino pins
+//       4  3  G  2  N    <-- Arduino pins
 //                   C
 //
 // ATTiny24/44/84 Microcontroller
 //
 //                           +-\/-+
 //                     VCC  1|    |14  GND
-//             (D  0)  PB0  2|    |13  AREF (D 10)
-//             (D  1)  PB1  3|    |12  PA1  (D  9) 
-//                     PB3  4|    |11  PA2  (D  8) 
-//  PWM  INT0  (D  2)  PB2  5|    |10  PA3  (D  7) 
-//  PWM        (D  3)  PA7  6|    |9   PA4  (D  6) 
-//  PWM        (D  4)  PA6  7|    |8   PA5  (D  5)        PWM
+//              (D10)  PB0  2|    |13  AREF (D0)
+//               (D9)  PB1  3|    |12  PA1  (D1) 
+//        RST          PB3  4|    |11  PA2  (D2) 
+//  PWM  INT0    (D8)  PB2  5|    |10  PA3  (D3) 
+//  PWM          (D7)  PA7  6|    |9   PA4  (D4) SCL
+//  PWM  MOSI    (D6)  PA6  7|    |8   PA5  (D5) MOSI   PWM
 //                           +—--—+
 
-// Array of bitmasks encoding indivudual segements needed on the dispaly to represent the number at the array index
+// Array of bitmasks encoding indivudual segements needed on the display to represent the number at the array index
 byte numbers[] = {B11111100,  //0
                   B01100000,  //1
                   B11011010,  //2
@@ -62,19 +64,19 @@ byte numbers[] = {B11111100,  //0
 byte output_number(byte n) {
 
   byte num = numbers[n];
-  digitalWrite( 4, B10000000 & num);
-  digitalWrite( 5, B01000000 & num);
-  digitalWrite( 6, B00100000 & num); 
-  digitalWrite( 7, B00010000 & num); 
-  digitalWrite( 8, B00001000 & num); 
-  digitalWrite( 9, B00000100 & num);
-  digitalWrite(10, B00000010 & num);
+  digitalWrite(0, B10000000 & num); //a
+  digitalWrite(1, B01000000 & num); //b
+  digitalWrite(2, B00100000 & num); //c
+  digitalWrite(3, B00010000 & num); //d
+  digitalWrite(4, B00001000 & num); //e
+  digitalWrite(5, B00000100 & num); //f
+  digitalWrite(6, B00000010 & num); //g
 }
 
 void setup() {
 
-  // Setup pins 4-10 for output
-  for (int i = 4; i <= 10; i++) pinMode(i, OUTPUT);
+  // Setup pins 0-6 for output
+  for (int i = 0; i <= 6; i++) pinMode(i, OUTPUT);
 }
 
 void loop() {
